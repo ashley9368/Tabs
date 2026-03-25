71dc924cbefe01795d8d8ef80fa3f389036bc942
ashley9368 <ashleygoward@icloud.com>
2026-03-25 10:55:34 +0000

    add onclick listener and print each click to the console

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const target = document.querySelectorAll(".tab")
+const button = document.querySelectorAll(".button-main")
 
 for (let i = 0; i < 4; i++) {
     target[i].style.display = "none";
 
     console.log(target[i])
-} 
+}
+
+target[0].style.display = "block";
+
+button[0].onclick = function() {
+    console.log("click")
+}
